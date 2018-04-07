f13d9b84109ad53890b0216a6206b42da256b42c
Okurakun <yuki-tasuku@outlook.jp>
2018-04-08 06:24:56 +0900

    PinBall 動作確認
    得点が加算されているか

---
+++ Assets/Lesson5/FripperTapControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperTapControl : MonoBehaviour {
+
+    private HingeJoint myHingeJoint;
+
+    private float defaultAngle = 20;
+
+    private float flickAngle = -20;
+
+    void Start () {
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        SetAngle(this.defaultAngle);
+    }
+	
+	
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                   
+                    SetAngle(this.flickAngle);
+
+                    break;
+
+                case TouchPhase.Ended:
+                    SetAngle(this.defaultAngle);
+                    break;
+            }
+             
+
+        }
+      
+
+
+    }
+
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/Lesson5/Score.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+  private GameObject ScoreText;
+
+    int Point = 0;
+
+
+	void Start () {
+        this.ScoreText = GameObject.Find("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("SmallStarTag"))
+        {
+            this.Point += 10;
+            this.ScoreText.GetComponent<Text>().text = ""+Point;
+        } else if (other.gameObject.CompareTag("LargeStarTag"))
+        {
+            this.Point += 15;
+            this.ScoreText.GetComponent<Text>().text = "" + Point;
+        } else if (other.gameObject.CompareTag("SmallCloudTag"))
+        {
+            this.Point += 20;
+            this.ScoreText.GetComponent<Text>().text = "" + Point;
+        } else if (other.gameObject.CompareTag("LargeCloudTag"))
+        {
+            this.Point += 25;
+            this.ScoreText.GetComponent<Text>().text = "" + Point;
+        }
+
+    }
+}
